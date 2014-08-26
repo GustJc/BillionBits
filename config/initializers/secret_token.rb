38debc0ff046f4b9a5d402f1a89339ec441810d8
Gust <darksshades@hotmail.com>
2014-08-25 21:25:56 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BillionBits::Application.config.secret_token = '6ef2332e69eec2f13e92bdfbd30f0f704e89310917db28895c5897488896f7524d72dd07cc39bbc460396c537124e32d6d8ecd9a97e396685f65d5c9bfcaefd6'
